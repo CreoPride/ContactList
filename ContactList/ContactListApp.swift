2a6975f2332d9b4116e01460d6d6b3f747d9bc42
Konstantin Fomenkov <kolij2005@gmail.com>
2022-05-27 18:50:09 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Konstantin Fomenkov on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
